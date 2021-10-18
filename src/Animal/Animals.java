2b128862a756af5737db8e1a00b573a71fd81f30
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2021-10-18 13:39:00 +0200

    added lines

--- src/Animal/Animals.java
+++ src/Animal/Animals.java
@@ -2,10 +2,23 @@ package Animal;
 
 public abstract class Animals {
     private String name;
+    private String gender;
+    private double health;
 
     public Animals(String name){
         this.name = name;
 
+
+    }
+    public void eat(){
+
+    }
+    public void sayHi(){
+        System.out.println("Hello");
+    }
+
+    public double getHealth() {
+        return health;
     }
 }
 
--- src/AnimalGame/Game.java
+++ src/AnimalGame/Game.java
@@ -1,9 +1,12 @@
 package AnimalGame;
 
+
 import java.util.Scanner;
 
 public class Game {
     private Scanner console;
+
+
 
     public Game(){
         this.console = new Scanner(System.in);
@@ -12,6 +15,15 @@ public class Game {
     public void startGame(){
         System.out.println("Välkommen till AnimalGame! Tryck enter för att starta...");
         console.nextLine();
+        System.out.println("(5-30)");
+        System.out.println("Ange antalet spelrundor:");
+        console.nextInt();
+        System.out.println("(2-4)");
+        System.out.println("Ange antalet spelare:");
+        console.nextInt();
+        System.out.println("Ange namn för spelare 1:");
+
+
 
     }
 }
