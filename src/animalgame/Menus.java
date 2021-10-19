2395eec4de119d90b829b2aa131ec35101277f6b
Dinvej16 <91056406+Dinvej16@users.noreply.github.com>
2021-10-19 09:56:47 +0200

    added stuff

---
+++ src/animalgame/Menus.java
@@ -0,0 +1,16 @@
+package animalgame;
+
+public class Menus {
+
+    public void printStoreMenu(){
+        System.out.println();
+        System.out.println("1.Köp Djur, 2.Köp Mat, 3.Sälj djur, 4.Dina djur, 5.Din mat ");
+        System.out.println("Mata in en siffra för att göra ett val: ");
+    }
+    public void printPlayerMenu(){
+        System.out.println();
+        System.out.println("1.Mata djur,. 2.Para djur, 3.Affär ");
+        System.out.println("Mata in en siffra för att göra ett val:");
+    }
+
+}
